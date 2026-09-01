02c4e5dbfb5f569f901cee72e0f642a654d571be
michaelmo12 <mordohmichael9@gmail.com>
2026-09-01 16:05:59 +0300

    header for func waiting for monitor to send file

--- tx/sender/include/sender/sender.hpp
+++ tx/sender/include/sender/sender.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 /**
  * Creates, binds, and starts listening on a Unix domain socket at the given path.
  *
@@ -7,3 +9,11 @@
  * @return The socket's file descriptor, or -1 on failure
  */
 int setup_ipc_socket(const char* socket_path);
+
+/**
+ * Blocks until File Monitor connects and sends a message.
+ *
+ * @param ipc_fd File descriptor of a socket already listening (from setup_ipc_socket)
+ * @return The message received from File Monitor
+ */
+std::string wait_for_file_monitor(int ipc_fd);
