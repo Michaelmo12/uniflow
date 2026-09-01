3681788653af233cf6a334c6174408f4ee8f022f
michaelmo12 <mordohmichael9@gmail.com>
2026-09-01 16:40:12 +0300

    max ipc message size in config

--- tx/sender/include/sender/config.hpp
+++ tx/sender/include/sender/config.hpp
@@ -6,3 +6,6 @@ constexpr const char* IPC_SOCKET_PATH = "/tmp/uniflow_monitor_to_sender.sock";
 // destination for the UDP packets sent to Receiver
 constexpr const char* RECEIVER_IP = "127.0.0.1";
 constexpr int RECEIVER_PORT = 5004;
+
+// Max size of a single IPC message, in bytes
+constexpr int MAX_IPC_MESSAGE_SIZE = 1024;
