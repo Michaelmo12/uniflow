344680d44ee927d7502a143c507fc780a05c2527
michaelmo12 <mordohmichael9@gmail.com>
2026-09-01 16:05:29 +0300

    corrected port for receiver

--- tx/sender/include/sender/config.hpp
+++ tx/sender/include/sender/config.hpp
@@ -5,4 +5,4 @@ constexpr const char* IPC_SOCKET_PATH = "/tmp/uniflow_monitor_to_sender.sock";
 
 // destination for the UDP packets sent to Receiver
 constexpr const char* RECEIVER_IP = "127.0.0.1";
-constexpr int RECEIVER_PORT = 5005;
+constexpr int RECEIVER_PORT = 5004;
