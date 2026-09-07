eb5ebfb4803043d44d18d18b50210c1c111aa6bd
michaelmo12 <mordohmichael9@gmail.com>
2026-09-07 11:21:13 +0300

    config change

--- tx/sender/include/sender/config.hpp
+++ tx/sender/include/sender/config.hpp
@@ -6,8 +6,8 @@
 constexpr const char* IPC_SOCKET_PATH = "/tmp/uniflow_monitor_to_sender.sock";
 
 // destination for the UDP packets sent to Receiver
-constexpr const char* RECEIVER_IP = "127.0.0.1";
-constexpr int RECEIVER_PORT = 5004;
+constexpr const char* RECEIVER_IP = "10.126.146.57";
+constexpr int RECEIVER_PORT = 5005;
 
 // Max size of a single IPC message, in bytes
 constexpr int MAX_IPC_MESSAGE_SIZE = 1024;
